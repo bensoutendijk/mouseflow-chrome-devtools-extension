6cd87a25300f54264af957b86682fd1f79ef1784
Ben Soutendijk <devaent1316@gmail.com>
2019-02-08 21:56:18 -0600

    add styles

--- popup.js
+++ popup.js
@@ -1,13 +1,23 @@
+var mfData = document.getElementById('mouseflow-data');
 var mfIsRecording = document.getElementById('mouseflow-is-recording');
 var mfRecordingRate = document.getElementById('mouseflow-recording-rate');
 var mfWebsiteId = document.getElementById('mouseflow-website-id');
 var mfSessionId = document.getElementById('mouseflow-session-id');
+var mfViewRecordingLink = document.getElementById('mouseflow-view-recording-link');
+var sessionLink = document.getElementById('mouseflow-session-link')
+var mfNotInstalled = document.getElementById('mouseflow-not-installed');
 
 chrome.runtime.onMessage.addListener(function(mf, sender) {
   if (sender.tab.active) {
-    mfIsRecording.innerText = mf.isRecording;
-    mfRecordingRate.innerText = mf.recordingRate;
-    mfWebsiteId.innerText = mf.websiteId;
-    mfSessionId.innerText = mf.sessionId;
+    if(mf.isInstalled) {
+      mfIsRecording.innerText = mf.isRecording ? 'On' : 'FALSE';
+      mfRecordingRate.innerText = mf.recordingRate;
+      mfWebsiteId.innerText = mf.websiteId;
+      mfSessionId.innerText = mf.sessionId;
+      sessionLink.href = `https://app.mouseflow.com/websites/${mf.websiteId}/recordings/${mf.sessionId}/play`;
+      mfData.style = 'display: block';
+    } else {
+      mfNotInstalled.style = "display: block";
+    }
   }
 });
