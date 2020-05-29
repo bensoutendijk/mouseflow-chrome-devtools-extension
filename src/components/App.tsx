50251b28bc2c4e9971d947eb5305574502303121
bensoutendijk <bensoutendijk@gmail.com>
2020-05-29 16:19:21 -0500

    add module for DiagnosticsCard

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Header';
+import DiagnosticsCard from './DiagnosticsCard';
 
 import { MessageData } from '../types';
 
@@ -31,80 +32,18 @@ const App = function() {
     return null;
   }
 
-  const copyInput: React.MouseEventHandler = function(event) {
-    event.preventDefault();
-    
-    const element = event.target as HTMLButtonElement;
-    const dataTarget = element.getAttribute('data-target');
-    if (dataTarget === null) {
-      return;
-    }
-
-    const target = document.getElementById(dataTarget) as HTMLInputElement;
-    if (target === null) {
-      return;
-    }
-    
-    // Copy the text
-    target.select();
-    document.execCommand('copy');
-    
-    // Change the button text temporarily
-    const originalText = element.innerText;
-    element.innerText = 'Copied';
-    setTimeout(() => { 
-      element.innerText = originalText;
-    }, 2000);
-  };
-
   return (
     <div className="App">
       <div className="container mt-4">
         <Header version={state.data.version} isInstalled={state.data.isInstalled} />
         {state.data.isInstalled ? (
           <div>
-            <div className="Diagnostics card">
-              <div className="card-header">
-                <em>{`Diagnostics`}</em>
-              </div>
-              <ul className="diagnostics-list list-group list-group-flush">
-                <li className="list-group-item">
-                  <div>
-                    {`Recording: `}<span className={state.data.isRecording ? "text-danger" : "text-secondary"}>{`${state.data.isRecording}`}</span>
-                  </div>
-                  <a 
-                    className="btn btn-info"
-                    href={`https://app.mouseflow.com/websites/${state.data.websiteId}/recordings/${state.data.sessionId}/play`} 
-                    target="_blank"
-                  >
-                    {`View session`}<img className="ml-2" style={{ width: '1em' }} src="assets/images/external-link-duotone.svg" />
-                  </a>
-                </li>
-                <li className="list-group-item">
-                  <div>
-                    {`Recording rate: `}<span className="text-info">{`${state.data.recordingRate}%`}</span>
-                  </div>
-                </li>
-                <li className="list-group-item">
-                  <div className="input-group mr-1">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text">{`Website ID: `}</span>
-                    </div>
-                    <input type="text" id="website-id" className="form-control" value={`${state.data.websiteId}`}/>
-                  </div>
-                  <button type="button" className="btn btn-secondary" onClick={copyInput} data-target="website-id">Copy</button>
-                </li>
-                <li className="list-group-item">
-                  <div className="input-group mr-1">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text">{`Session ID: `}</span>
-                    </div>
-                    <input type="text" id="session-id" className="form-control" value={`${state.data.sessionId}`}/>
-                  </div>
-                  <button type="button" className="btn btn-secondary" onClick={copyInput} data-target="session-id">Copy</button>
-                </li>
-              </ul>
-            </div>
+            <DiagnosticsCard 
+              isRecording={state.data.isRecording}
+              recordingRate={state.data.recordingRate}
+              websiteId={state.data.websiteId}
+              sessionId={state.data.sessionId}
+            />
           </div>
         ) : null}
       </div>
---
+++ src/components/DiagnosticsCard.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+interface DiagnosticsCardProps {
+  isRecording?: boolean;
+  websiteId?: string;
+  sessionId?: string;
+  recordingRate?: number;
+}
+
+const DiagnosticsCard: React.FC<DiagnosticsCardProps> = function({ isRecording, websiteId, sessionId, recordingRate }) {
+
+  const copyInput: React.MouseEventHandler = function(event) {
+    event.preventDefault();
+            
+    const element = event.target as HTMLButtonElement;
+    const dataTarget = element.getAttribute('data-target');
+    if (dataTarget === null) {
+      return;
+    }
+        
+    const target = document.getElementById(dataTarget) as HTMLInputElement;
+    if (target === null) {
+      return;
+    }
+            
+    // Copy the text
+    target.select();
+    document.execCommand('copy');
+            
+    // Change the button text temporarily
+    const originalText = element.innerText;
+    element.innerText = 'Copied';
+    setTimeout(() => { 
+      element.innerText = originalText;
+    }, 2000);
+  };
+
+  return (
+    <div className="Diagnostics card">
+      <div className="card-header">
+        <em>{`Diagnostics`}</em>
+      </div>
+      <ul className="diagnostics-list list-group list-group-flush">
+        <li className="list-group-item">
+          <div>
+            {`Recording: `}<span className={isRecording ? "text-danger" : "text-secondary"}>{`${isRecording}`}</span>
+          </div>
+          <a 
+            className="btn btn-info"
+            href={`https://app.mouseflow.com/websites/${websiteId}/recordings/${sessionId}/play`} 
+            target="_blank"
+          >
+            {`View session`}<img className="ml-2" style={{ width: '1em' }} src="assets/images/external-link-duotone.svg" />
+          </a>
+        </li>
+        <li className="list-group-item">
+          <div>
+            {`Recording rate: `}<span className="text-info">{`${recordingRate}%`}</span>
+          </div>
+        </li>
+        <li className="list-group-item">
+          <div className="input-group mr-1">
+            <div className="input-group-prepend">
+              <span className="input-group-text">{`Website ID: `}</span>
+            </div>
+            <input type="text" id="website-id" className="form-control" value={`${websiteId}`}/>
+          </div>
+          <button type="button" className="btn btn-secondary" onClick={copyInput} data-target="website-id">Copy</button>
+        </li>
+        <li className="list-group-item">
+          <div className="input-group mr-1">
+            <div className="input-group-prepend">
+              <span className="input-group-text">{`Session ID: `}</span>
+            </div>
+            <input type="text" id="session-id" className="form-control" value={`${sessionId}`}/>
+          </div>
+          <button type="button" className="btn btn-secondary" onClick={copyInput} data-target="session-id">Copy</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default DiagnosticsCard;
