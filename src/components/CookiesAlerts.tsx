a65bb021a6506fc86960a715032f9d823a6ea99d
bensoutendijk <bensoutendijk@gmail.com>
2020-06-09 15:51:24 -0500

    wording

--- src/components/CookiesAlerts.tsx
+++ src/components/CookiesAlerts.tsx
@@ -8,12 +8,12 @@ const CookiesAlerts: React.FC<CookiesAlertsProps> = function({ cookies }) {
   return (
     <div className="CookiesAlerts">
       {typeof cookies.mfUser === 'undefined' ? (
-        <div className="alert alert-danger">mouseflow user cookie is not set.</div>
+        <div className="alert alert-danger">User cookie is not set.</div>
       ) : (
         null
       )}
       {typeof cookies.mfSession === 'undefined' ? (
-        <div className="alert alert-danger">mouseflow session cookie is not set.</div>
+        <div className="alert alert-danger">Session cookie is not set.</div>
       ) : (
         null
       )}
